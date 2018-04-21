9f443017845df783ce3a3299d8d4866493db4feb
Pedro Beani <bunnyzones25@gmail.com>
2018-04-21 14:15:37 -0300

    Tratando resposta
    Adicionando inteceptors para tratar erro

--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 import axios from '../../axios-orders'
 import Spinner from '../../components/UI/Spinner/Spinner'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -39,7 +40,7 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continue!')
-
+        this.setState({ loading: true })
         const order = {
             ingredients: this.state.ingredients,
             price: this.state.price,
@@ -55,8 +56,12 @@ class BurgerBuilder extends Component {
             deliveryMethod: 'fast'
         }
         axios.post('/orders.json', order)
-            .then(response => console.log(response))
-            .catch(error => console.log(error))
+            .then(response => {
+                this.setState({ loading: false, purchasing: false })
+            })
+            .catch(error => {
+                this.setState({ loading: false, purchasing: false })
+            })
     }
 
     updatePurchaseState(ingredients) {
@@ -122,7 +127,7 @@ class BurgerBuilder extends Component {
                 price={this.state.totalPrice} />
         )
 
-        if(this.state.loading) {
+        if (this.state.loading) {
             orderSummary = <Spinner />
         }
 
@@ -147,4 +152,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder
+export default withErrorHandler(BurgerBuilder, axios)
