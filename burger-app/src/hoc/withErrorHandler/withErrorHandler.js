5e9adbc6e01c962ead6969f3325b3d643fb9c765
Pedro Beani <bunnyzones25@gmail.com>
2018-04-22 00:24:13 -0300

    Melhoria
    Limpando Interceptors

--- burger-app/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger-app/src/hoc/withErrorHandler/withErrorHandler.js
@@ -11,13 +11,17 @@ const withErrorHandler = (WrappedComponent, axios) => {
             }
 
             componentWillMount() {
-                axios.interceptors.request.use(req => {
-                    this.setState({ error: null})
+                this.requestInterceptor = axios.interceptors.request.use(req => {
+                    this.setState({ error: null })
                     return req
                 })
-                axios.interceptors.response.use(res => res, error => {
-                    this.setState({ error: error})
+                this.responseInterceptor = axios.interceptors.response.use(res => res, error => {
+                    this.setState({ error: error })
                 })
+            }
+
+            componentWillUnmount() {
+
             }
 
             confirmErrorHandler = () => {
@@ -27,7 +31,7 @@ const withErrorHandler = (WrappedComponent, axios) => {
             render() {
                 return (
                     <Aux>
-                        <Modal 
+                        <Modal
                             show={this.state.error}
                             modalClosed={this.confirmErrorHandler}>
                             {this.state.error ? this.state.error.message : null}
